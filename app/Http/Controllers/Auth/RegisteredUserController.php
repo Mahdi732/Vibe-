6fa444cbe481ec83614b8cb01c958bfc494ef53a
Mahdi732 <ey449751@gmail.com>
2025-02-19 10:25:29 +0100

    add rounting test

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,12 +34,15 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'img' => ['required', 'max:2048'],
+
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'picture' => $request->file('img')->store('pictures', 'public'),
         ]);
 
         event(new Registered($user));
