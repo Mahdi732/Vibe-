fd6327a16b17a746f82ac1460454f8966a164f72
Mahdi732 <ey449751@gmail.com>
2025-02-21 11:00:04 +0100

    Add Test model with static methods
    Introduced a new `Test` class in the `App\Models` namespace. Includes static methods `questoin` and `find` as initial implementations for testing purposes.

---
+++ app/Models/test.php
@@ -0,0 +1,22 @@
+ //  
+//
+ //   namespace App\Models;
+//
+ //   class Test {
+//
+//        public static function questoin() {
+//            return [
+//                'Quick question' => 'why are you gay'
+//            ];
+//        }
+//
+ //       public static function find($id) {
+ //           $test = self::all();
+  //          foreach($test as $test){
+  //              if($test["id"] == $id){
+ //                   return $test;
+ //               }
+ //           }
+ //       }
+ //       
+ //   }
