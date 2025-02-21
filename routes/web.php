7689b8d775e18f5517f6c89ca3875216f00396de
Mahdi732 <ey449751@gmail.com>
2025-02-21 10:59:12 +0100

    Add login route and clean up commented test routes
    Introduced a new login route pointing to the AuthenticatedSessionController. Commented out unnecessary or unused test and debug routes to improve code clarity and maintainability.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Models\test;
+use Illuminate\Auth\Events\Authenticated;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,15 +31,25 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get("/login" , [AuthenticatedSessionController::class , 'create']);
 
-Route::get('/vibe', function(Request $request){
-    $ne = '<h1>vibe  BUT not my type but wisssam is exception</h1>';
-    dd($ne);
-    return '<h1>vibe  BUT not my type but wisssam is exception</h1>';
-});
+// Route::get('/vibe', function(Request $request){
+//     $ne = '<h1>vibe  BUT not my type but wisssam is exception</h1>';
+//     dd($ne);
+//     return '<h1>vibe  BUT not my type but wisssam is exception</h1>';
+// });
 
-Route::get('/dark', function(Request $request){
-    dd ($request);
-});
+// Route::get('/dark', function(Request $request){
+//     return view('dark', [
+//         'test' => Test::questoin()
+//     ]);
+// });
+
+// Route::get('/dark/{id}', function($id) {
+//     return view('dark', [
+//         'test' => Test::find($id)
+//     ]);
+// });
+ 
 
 require __DIR__.'/auth.php';
