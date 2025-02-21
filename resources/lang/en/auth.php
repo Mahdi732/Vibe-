039933e448f1bfe566f5decc97daf83367f74cea
Mahdi732 <ey449751@gmail.com>
2025-02-21 10:59:52 +0100

    Add auth language file and placeholder users table migration
    Added a new English language file for authentication strings to support localization. Also included a placeholder migration file for future updates to the users table.

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'login' => 'auth.login',
+    'password' => 'Password',
+    'remember_me' => 'Remember me',
+    'forgot_password' => 'Forgot your password?',
+    'test' => 'test zabba'
+];
